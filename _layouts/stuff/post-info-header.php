68d8191d1ad1b2a990870b033ecac304cca4cd78
Eduardo Ferreira <mulberry.beacon@gmail.com>
2013-06-03 16:33:32 +0100

    Removed files with migrated content. Updated both header and footer of main page. Added social network buttons for posts.

--- _layouts/stuff/post-info-header.php
+++
@@ -1,9 +0,0 @@
-<?php /* Header section with date/time and author information, and social sharing link */ ?>
-			<div class="post-info">
-				<div class="meta-info">
-					<?php vint3_entry_meta(); ?>
-				</div>
-				<div class="social-share">
-					<div class="g-plusone" data-href="<?php the_permalink(); ?>" data-recommendations="false" data-size="medium"></div>
-				</div>
-			</div>
